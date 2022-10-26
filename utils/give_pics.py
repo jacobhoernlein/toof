d6a64dbf4f4635a2d71195162dd22850e8142da7
jakemrh <jacobhoernlein@gmail.com>
2022-10-25 21:59:11 -0500

    moving utils

--- utils/give_pics.py
+++ utils/give_pics.py
@@ -7,18 +7,24 @@ from toof.cogs.pics import ToofPic, ToofPics
 
 
 if __name__ == "__main__":
-    user_id = 978022589882568794
+    user_id = int(sys.argv[2])
 
     conn = sqlite3.connect(sys.argv[1])
     cursor = conn.execute("SELECT * FROM pics WHERE user_id = 0")
-    all_pics = ToofPics([ToofPic(row[1], row[2]) for row in cursor])
+    all_pics = ToofPics([
+        ToofPic(row[1], row[2], row[3], row[4]) 
+        for row in cursor
+    ])
 
     cursor = conn.execute(f"SELECT * FROM pics WHERE user_id = {user_id}")
-    user_collection = ToofPics([ToofPic(row[1], row[2]) for row in cursor])
+    user_pics = ToofPics([
+        ToofPic(row[1], row[2], row[3], row[4])
+        for row in cursor
+    ])
     cursor.close()
 
     for pic in all_pics:
-        if pic not in user_collection and pic.rarity.name == "rare":
+        if pic not in user_pics and pic.rarity.name == "rare":
             conn.execute(f"INSERT INTO pics VALUES ({user_id}, '{pic.id}', '{pic.link}')")
     conn.commit()
     conn.close()
